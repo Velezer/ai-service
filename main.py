f72af76e455d0641dd4c91edfca9f9bada9badaa
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2026-02-21 19:15:25 +0000

    stream

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from llama_cpp import Llama
 from dotenv import load_dotenv
+from fastapi.responses import StreamingResponse
+
 
 load_dotenv()
 
@@ -15,12 +17,14 @@ N_THREADS = int(os.getenv("N_THREADS", 1))
 N_BATCH = int(os.getenv("N_BATCH", 128))
 TEMPERATURE = float(os.getenv("TEMPERATURE", 0.7))
 TOP_P = float(os.getenv("TOP_P", 0.9))
+USE_MLOCK = bool(os.getenv("USE_MLOCK", True))
 
 llm = Llama(
     model_path=MODEL_PATH,
     n_ctx=N_CTX,
     n_threads=N_THREADS,
-    n_batch=N_BATCH
+    n_batch=N_BATCH,
+    use_mlock=USE_MLOCK
 )
 
 class GenerateRequest(BaseModel):
@@ -39,3 +43,38 @@ def generate(req: GenerateRequest):
     return {
         "response": output["choices"][0]["text"]
     }
+
+@app.post("/generate/stream/plain")
+def generate(req: GenerateRequest):
+
+    def stream():
+        for chunk in llm(
+            req.prompt,
+            max_tokens=MAX_TOKENS,
+            temperature=TEMPERATURE,
+            top_p=TOP_P,
+            stream=True,
+        ):
+            token = chunk["choices"][0]["text"]
+            if token:
+                yield token
+
+    return StreamingResponse(stream(), media_type="text/plain")
+
+
+@app.post("/generate/stream/event")
+def generate(req: GenerateRequest):
+
+    def stream():
+        for chunk in llm(
+            req.prompt,
+            max_tokens=MAX_TOKENS,
+            temperature=TEMPERATURE,
+            top_p=TOP_P,
+            stream=True,
+        ):
+            token = chunk["choices"][0]["text"]
+            if token:
+                yield f"data: {token}\n\n"
+
+    return StreamingResponse(stream(), media_type="text/event-stream")
